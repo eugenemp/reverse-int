258372f98041418fae44cf3368f24a2765fe9b51
eugenemp <phoenixlp91@mail.ru>
2021-02-24 20:36:47 +0300

    feat: implement function reverse()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+
+    //Convert number to the array of string items,
+    //reversing array and returning joined digits to the variable as a string
+    let str = n.toString().split('').reverse().join('');
+
+    //Converting string value to number
+    return parseInt(str, 10);
 }
